0445d7361a5cdd888fc76718c5f37ea4b5dfcff2
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-05-10 11:43:17 +0200

    Update projectList.svelte
    - add gridview
    - add css
    - add sorting (untested)*
    - todo: make sure 'getSortedProjects' is working
    - todo: maybe rename 'getSortedProjects' to something more descriptive (sortProjectsBy)
    -  todo: make a getter of all the sortBy values and make a map of them.
    
    * Currently not working. Prob due to some of the values not existing. e.g., popular, relevancy, price, closest..
    However 'date published' should be working, so i have to check if it fetches the correct information in order to sort them.
    todo: make a

--- src/stories/projectList.svelte
+++ src/stories/projectList.svelte
@@ -1,79 +1,108 @@
-<!-- USED BY projectListPage, this is the list that contains the company cards (search results) -->
-<script>
-	import ProjectCard from './projectCard.svelte';
+<svelte:options runes />
 
-	// import './form.css';
+<script lang="ts">
+	import ProjectCard from './ProjectCard.svelte';
+	import { writable, derived } from 'svelte/store';
+	import { projects } from './data/projectData';
+	// ! bug
+	// TODO [ ]: fix bug; the favorite icon are not unique. if i favorite the first project  page 1, but when next page; the first project on page 2 is also favoritted.
+	const props = $props<{
+		project?: any;
+		gridView: boolean;
+		sortBy?: string;
+	}>();
 
-	const demo_project = {
-		area: 'Vestland, Bergen',
-		job_title: 'Bygge terasse rundt baksiden',
-		category: 'Carpenter',
-		sub_category: 'none',
-		lister_class: 'private',
-		small_description:
-			'Du har bachelorgrad eller mastergrad i regnskap og revisjon, og er gjerne statsautorisert revisor. Annen økonomiutdanning på mastergradsnivå kan også være aktuelt. Søkere med utdanning tatt i utlandet må vedlegge dokumentasjon fra HK-dir/NOKUT som bekrefter utdanningens omfang og nivå sammenlignet med tilsvarende norsk utdanning.',
-		description:
-			'Som et resultat av ny teknologi er revisjon i stor endring. Riksrevisjonen har utviklet egne dataanalyser og vil fortsette å ta i bruk mulighetene som den nye teknologien gir oss. Du får muligheten til å jobbe tett med kolleger i grenseflaten mellom teknologi og revisjon.Vi legger vekt på kompetanseutvikling og har gode stipendordninger for etter- og videreutdanning. Du får tilbud om medlemskap i revisorforeningens kurspakke, Rekomp. Hvis du ønsker å bli statsautorisert revisor kan du opptjene den nødvendige veiledede praksis hos oss. Vi har fleksitid og sommertid, og hver uke får du en time til å trene i arbeidstiden. Du blir medlem i Statens pensjonskasse som innebærer en god tjenestepensjon, yrkesskade- og gruppelivsforsikring og mulighet for gunstig boliglån.Du vil bli ansatt som rådgiver med lønnsspenn fra 590.000 kroner til 650.000 kroner, eller som seniorrådgiver med lønnsspenn fra 650.000 kroner til 915.000 kroner. For spesielt kvalifiserte kandidater kan høyere lønn vurderes.',
-		date_issued: '07.04.2025',
-		due_date: '15.05.2025',
-		heart: false,
-		payed_listing: false,
-		tags: ['flermannsjobb', 'har materiale', 'har ikke utstyr', 'maskin tilgang'],
-		// 'image_link':'https://oppdragsmarkedet.no/prosjekt/36373826/assets/thumbnail.jpg',
-		image_link:
-			'https://www.mycarpentry.com/image-files/xdeck-stair-house-perspective.jpg.pagespeed.ic.r3NyM_JrfO.webp',
-		project_link: 'https://oppdragsmarkedet.no/prosjekt/36373826'
-	};
+	const currentPage = writable(1);
+	const totalPages = Math.ceil(projects.length / 12);
 
-	function getProjects() {
-		var projects = Array(1).fill(demo_project);
-		projects.push({
-			area: 'END OF LIST',
-			job_title: 'END OF LIST',
-			category: 'carpenter',
-			lister_class: 'private',
-			date_issued: '07.04.2025',
-			due_date: '15.05.2025',
-			heart: false,
-			payed_listing: false,
-			tags: ['flermanns_jobb', 'har_materiale', 'har_ikke_utstyr', 'maskin_tilgang'],
-			image_link: 'https://oppdragsmarkedet.no/prosjekt/36373826/assets/thumbnail.jpg',
-			project_link: 'https://oppdragsmarkedet.no/prosjekt/36373826'
-		});
-		// console.log(projects)
-		return projects;
-	}
-
-	const projects = getProjects();
-	let currentPage = 1;
-	const totalPages = Math.ceil(projects.length / 10);
-
-	function changePage(page) {
+	function changePage(page: number) {
 		if (page >= 1 && page <= totalPages) {
-			currentPage = page;
+			currentPage.set(page);
 		}
 	}
+	// TODO [] make sure that this is working, it doesn't seem like it is.
+	// > copilot referance: could you make the sortby dropdown to actually work?
+	// Apply sorting based on sortBy parameter
+	function getSortedProjects(projectList: any[]) {
+		const sortedProjects = [...projectList];
 
-	$: visibleProjects = projects.slice((currentPage - 1) * 10, currentPage * 10); // Each page contains 10 cards
+		if (props.sortBy) {
+			switch (props.sortBy) {
+				case 'Mest relevant':
+					// Default sorting (no change required)
+					break;
+				case 'Publisert (ny til gammel)':
+					sortedProjects.sort((a, b) => {
+						// Parse dates and sort by newest first
+						const dateA = new Date(a.date_issued.split('.').reverse().join('-'));
+						const dateB = new Date(b.date_issued.split('.').reverse().join('-'));
+						return dateB.getTime() - dateA.getTime();
+					});
+					break;
+				case 'Publisert (gammel til ny)':
+					sortedProjects.sort((a, b) => {
+						// Parse dates and sort by oldest first
+						const dateA = new Date(a.date_issued.split('.').reverse().join('-'));
+						const dateB = new Date(b.date_issued.split('.').reverse().join('-'));
+						return dateA.getTime() - dateB.getTime();
+					});
+					break;
+				case 'Pris (høy til lav)':
+					// If price data becomes available, sort by price high to low
+					// Currently no price data in the project objects
+					break;
+				case 'Pris (lav til høy)':
+					// If price data becomes available, sort by price low to high
+					// Currently no price data in the project objects
+					break;
+				case 'Nærmest':
+					// This would require geolocation data, currently using due date as a proxy for "nearest"
+					sortedProjects.sort((a, b) => {
+						// Parse due dates and sort by closest deadline first
+						const dateA = new Date(a.due_date.split('.').reverse().join('-'));
+						const dateB = new Date(b.due_date.split('.').reverse().join('-'));
+						return dateA.getTime() - dateB.getTime();
+					});
+					break;
+				default:
+					// No sorting (default behavior)
+					break;
+			}
+		}
+
+		return sortedProjects;
+	}
+
+	// Computed value with runes - now with sorting applied
+	const visibleProjects = derived(currentPage, ($currentPage) => {
+		const sortedProjects = getSortedProjects(projects);
+		return sortedProjects.slice(($currentPage - 1) * 12, $currentPage * 12);
+	});
+
+	console.log('in ProjectList, gridView:', props.gridView);
+	console.log('in ProjectList, sortBy:', props.sortBy);
 </script>
 
-<div class="project-list">
-	{#each visibleProjects as _project}
-		<ProjectCard project={_project}></ProjectCard>
+<div class="project-list" class:grid-view={props.gridView}>
+	{#each $visibleProjects as project}
+		<ProjectCard {project} gridView={props.gridView}></ProjectCard>
 	{/each}
-	<div class="page-list">
-		<button on:click={() => changePage(currentPage - 1)} disabled={currentPage === 1}
-			>Previous</button
-		>
+</div>
+
+<div class="pagination">
+	<button onclick={() => changePage($currentPage - 1)} disabled={$currentPage === 1}
+		>Previous</button
+	>
+	<div class="page-numbers">
 		{#each Array(totalPages)
 			.fill(0)
 			.map((_, i) => i + 1) as page}
-			<button on:click={() => changePage(page)} class:active={page === currentPage}>{page}</button>
+			<button onclick={() => changePage(page)} class:active={page === $currentPage}>{page}</button>
 		{/each}
-		<button on:click={() => changePage(currentPage + 1)} disabled={currentPage === totalPages}
-			>Next</button
-		>
 	</div>
+	<button onclick={() => changePage($currentPage + 1)} disabled={$currentPage === totalPages}
+		>Next</button
+	>
 </div>
 
 <style>
@@ -84,24 +113,93 @@
 		flex-wrap: nowrap;
 		align-content: flex-start;
 		justify-content: flex-start;
-		align-items: flex-start;
+		/* align-items: flex-start; */
 		width: 100%;
-		padding: 2rem;
+		/* padding: 2rem; */
 		row-gap: 2rem;
+		max-width: 100%;
+		/* overflow: hidden; */
+		padding-top: 2rem;
+		/* padding-bottom: 0.5rem;
+		padding-right: 0.5rem;
+		padding-left: 0.5rem; */
 	}
 
-	.page-list button {
+	.project-list.grid-view {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr); /* Default: 2 columns */
+		gap: 2rem;
+		width: 100%;
+	}
+
+	/* Media queries for responsive grid view */
+	@media (min-width: 1200px) {
+		.project-list.grid-view {
+			grid-template-columns: repeat(3, 1fr);
+		}
+	}
+
+	@media (max-width: 1000px) {
+		.project-list.grid-view {
+			grid-template-columns: repeat(2, 1fr);
+			/* grid-template-columns: repeat(3, 1fr); */
+		}
+	}
+
+	@media (max-width: 630px) {
+		.project-list.grid-view {
+			grid-template-columns: 1fr;
+			/* grid-template-columns: 1fr; */
+		}
+	}
+
+	.grid-view :global(.main.card) {
+		flex-direction: column;
+		height: auto;
+		row-gap: 0;
+		/* min-width: 250px; */
+		/* max-width: 100%; */
+	}
+
+	.grid-view :global(.thumbnail) {
+		width: 100%;
+		height: 200px;
+		min-width: unset;
+	}
+
+	.grid-view :global(.main.card .mini-profile.container) {
+		width: 100%;
+	}
+
+	.pagination {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		margin-top: 2rem;
+		width: 100%;
+	}
+
+	.pagination .page-numbers {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		flex-wrap: wrap;
+		margin: 0 1rem;
+	}
+
+	.pagination button {
 		margin: 0 5px;
 		padding: 5px 10px;
 		cursor: pointer;
 	}
 
-	.page-list button.active {
+	.pagination button.active {
 		font-weight: bold;
 		text-decoration: underline;
 	}
 
-	.page-list button:disabled {
+	.pagination button:disabled {
 		cursor: not-allowed;
 		opacity: 0.5;
 	}
