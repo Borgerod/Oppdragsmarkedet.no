25b4070110b2b0faa023ca6a4aaa4d67338f229a
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-05-10 12:56:38 +0200

    Update +layout.svelte
    - add color_scheme and dark_mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
 
 <script lang="ts">
 	import '../app.css';
+	import '../color_scheme.css';
+	import '../dark-mode.css'; // Import the dark mode styles
 	let { children } = $props();
 
 	// import home from './home.svelte'
-
 </script>
 
 {@render children()}
