30f6df7428d8fb5781d5b19de3a72157264f0de6
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-05-10 12:06:12 +0200

    Update Footer.svelte
    - add more contact-icons
    - remove unused icons
    - move dark-mode functionality
    - fix layout
    - change div from "language-dark-light-mode-buttons" to "language-dark-mode-container"

--- src/stories/Footer.svelte
+++ src/stories/Footer.svelte
@@ -1,108 +1,102 @@
 <script lang="ts">
 	import './Footer.css';
+	import './forms/form.css';
+	import Button from './Button.svelte';
+	// Import our new DarkModeToggle component
+	import DarkModeToggle from './DarkModeToggle.svelte';
 
 	let locale = $state('en');
 
 	import { Fa } from 'svelte-fa';
-	import {
-		faEarthAmerica,
-		faEnvelope,
-		faEnvelopeSquare,
-		faLanguage,
-		faMoon,
-		faPhone,
-		faPhoneSquare,
-		faSun
-	} from '@fortawesome/free-solid-svg-icons';
+	import { faEarthAmerica, faEnvelope, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
 	import {
 		faGithub,
 		faFacebook,
-		faSquareFacebook,
 		faInstagram,
 		faLinkedin,
 		faTiktok,
-		faInstagramSquare,
 		faLinkedinIn,
 		faFacebookF
 	} from '@fortawesome/free-brands-svg-icons';
-	// import Button from './Button.svelte';
 
-	/**
-	 * @typedef {Object} Props
-	 * @property {{name: string}} [user] The user object
-	 * @property {() => void} [onLogin] The login event handler
-	 * @property {() => void} [onLogout] The logout event handler
-	 * @property {() => void} [onCreateAccount] The account creation event handler
-	 * @property {boolean} [darkmode] The dark mode state
-	 */
-
-	let darkmode = $state(false);
-
-	function handleDarkMode() {
-		darkmode = !darkmode;
-		if (darkmode) {
-			document.body.classList.add('dark-mode');
-		} else {
-			document.body.classList.remove('dark-mode');
-		}
-	}
 	const languages = ['en', 'no'];
+
+	function handleSubscribe() {
+		// > PLAVEHODLER
+		// todo [] make this
+	}
 </script>
 
 <footer class="footer">
-	<div class="col-1">
-		<h3 class="footer">USEFUL LINKS</h3>
-		<a href=""> About </a>
-		<a href=""> Services </a>
-		<a href=""> Contact </a>
-		<a href=""> Shop </a>
-		<a href=""> Blog </a>
-	</div>
-	<div class="col-2">
-		<h3 class="footer">NEWSLETTER</h3>
-		<form>
-			<input type="text" placeholder="Your Email Address" required />
-			<br />
-			<button type="submit">SUBSCRIBE</button>
-		</form>
-	</div>
-	<div class="col-3">
-		<h3 class="footer">CONTACT</h3>
-		<p>123, XYV Road, BSK 3 <br /> Bangalore, karantaka, IN</p>
+	<div class="row">
+		<div class="col">
+			<h3>USEFUL LINKS</h3>
+			<div class="url-list">
+				<a href="https://oppdragsmarkedet.no/about"> About </a>
+				<a href="https://oppdragsmarkedet.no/Services"> Services </a>
+				<a href="https://oppdragsmarkedet.no/partners"> Partners </a>
+				<a href="https://oppdragsmarkedet.no/contant"> Contact </a>
+				<a href="https://oppdragsmarkedet.no/blog"> Blog </a>
+			</div>
+		</div>
+		<div class="col newsletter-col">
+			<h3>NEWSLETTER</h3>
+			<form class="newsletter-form">
+				<input type="text" placeholder="Your Email Address" required class="field" />
+				<br />
+				todo: pick one
+				<Button
+					rounded
+					size="large"
+					label="SUBSCRIBE"
+					type="submit"
+					onclick={handleSubscribe}
+					osl
+					hollow
+				/>
 
-		<div class="social-icons">
-			<a class="social-button" href="/stonks"><Fa icon={faFacebookF} /></a>
-			<a class="social-button" href="/stonks"><Fa icon={faLinkedinIn} /></a>
-			<a class="social-button" href="/stonks"><Fa icon={faInstagram} /></a>
-			<a class="social-button" href="/stonks"><Fa icon={faTiktok} /></a>
-			<a class="social-button" href="/stonks"><Fa icon={faEnvelope} /></a>
-			<a class="social-button" href="/stonks"><Fa icon={faPhone} /></a>
+				<Button
+					rounded
+					size="large"
+					label="SUBSCRIBE"
+					type="submit"
+					onclick={handleSubscribe}
+					dark_bg
+				/>
+			</form>
+		</div>
+		<div class="col">
+			<h3>CONTACT</h3>
+			<p>123, XYV Road, BSK 3 <br /> Bangalore, karantaka, IN</p>
+
+			<div class="social-icons">
+				<a class="social-button" href="/stonks"><Fa icon={faFacebookF} /></a>
+				<a class="social-button" href="/stonks"><Fa icon={faLinkedinIn} /></a>
+				<a class="social-button" href="/stonks"><Fa icon={faInstagram} /></a>
+				<a class="social-button" href="/stonks"><Fa icon={faTiktok} /></a>
+				<a class="social-button" href="/stonks"><Fa icon={faEnvelope} /></a>
+				<a class="social-button" href="/stonks"><Fa icon={faPhoneSquare} /></a>
+			</div>
 		</div>
 	</div>
 
-	<div class="language-dark-light-mode-buttons">
+	<div class="language-dark-mode-container">
 		<div class="relative inline-block w-30">
 			<select
 				class="language-select appearance-none rounded border border-gray-300 pr-10 focus:outline-none"
 			>
 				{#each languages as lang}
-					<!-- <option class="options" style="color: var(--primary); background-color: var(--tertiary-bg);" value={lang}>{lang}</option> -->
 					<option class="options" value={lang}>{lang}</option>
 				{/each}
 			</select>
 
 			<!-- Custom icon -->
 			<div class="pointer-events-none absolute inset-y-0 right-3 flex items-center">
-				<Fa icon={faEarthAmerica} />
+				<Fa icon={faEarthAmerica} class="icon" />
 			</div>
 		</div>
-		<button type="button" onclick={handleDarkMode} aria-label="Toggle dark mode">
-			{#if darkmode}
-				<Fa icon={faSun} />
-			{:else}
-				<Fa icon={faMoon} />
-			{/if}
-		</button>
+		<!-- Replace the old button with our new component -->
+		<DarkModeToggle />
 	</div>
 </footer>
 
@@ -111,34 +105,46 @@
 		color: var(--primary);
 	}
 
+	/* Add hover effect for select options */
+	option:hover,
+	select option:hover,
+	select option:focus,
+	select option:active {
+		background-color: var(--accent-bright) !important;
+		color: white !important;
+	}
+
+	/* Fix for some browsers that don't support :hover on options */
+	select:focus option:checked {
+		background-color: var(--accent-bright) !important;
+		color: white !important;
+	}
+
 	.language-select {
 		color: var(--white);
 		padding-right: 45px;
 		padding-left: 10px;
 	}
 
-	.language-dark-light-mode-buttons {
+	.language-dark-mode-container {
 		padding: 0 !important;
 		position: absolute;
 		bottom: 0;
 		right: 1rem;
 		display: flex;
 		flex-direction: row;
-	}
-	:global(body.dark-mode) button {
-		color: white;
+		color: var(--secondary-bg);
 	}
 
-	:global(body.dark-mode) footer {
-		color: white;
-		background-color: var(--accent-deep-shade);
+	/* Add hover effect for the icon in the select dropdown */
+	.pointer-events-none {
+		transition: color 0.2s ease;
 	}
-	:global(body) {
+
+	.relative:hover .pointer-events-none {
+		color: var(--accent-bright);
 	}
-	:global(body.dark-mode) {
-		background-color: var(--primary);
-		color: #bfc2c7;
-	}
+
 	p {
 		color: var(--primary-bg);
 		font-size: small;
@@ -147,14 +153,72 @@
 		font-size: small;
 	}
 
-	h3 {
-		color: #bdada1;
+	footer h3 {
+		color: var(--secondary-bg);
 		font-size: 1rem;
+		justify-content: space-around;
+		padding: 2vw 0;
+		/* margin-top: 10px; */
 	}
 
-	.footer div {
-		padding: 1rem;
+	footer div {
+		/* padding: 1rem; */
 		text-align: center;
+	}
+	.language-dark-mode-container div {
+		padding: 1rem;
+	}
+
+	footer .row {
+		display: flex;
+		flex-direction: row;
+		column-gap: 2rem;
+		width: 100%;
+		justify-content: space-between;
+	}
+
+	.col {
+		flex: 1;
+		width: 25%;
+		max-width: 300px;
+		min-width: 180px;
+		padding: 0 15px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		row-gap: 1rem;
+	}
+	.newsletter-col {
+		flex: 2;
+		width: 50%;
+		max-width: 600px;
+	}
+
+	.newsletter-form {
+		width: 100%;
+		max-width: 400px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	/* Responsive adjustments */
+	@media (max-width: 768px) {
+		footer .row {
+			flex-direction: column;
+			align-items: center;
+		}
+
+		.col,
+		.newsletter-col {
+			width: 100%;
+			max-width: 100%;
+			margin-bottom: 2rem;
+		}
+
+		.newsletter-form {
+			max-width: 80%;
+		}
 	}
 
 	.icon-container {
@@ -166,27 +230,25 @@
 		height: 0.9rem;
 		width: 0.9rem;
 		background: #efefef;
-		color: var(--tertiary-bg);
+		color: var(--third-bg);
 		border-radius: 2px;
 	}
 	footer {
+		padding: 2rem 3rem 1rem 3rem;
 		width: 100%;
 		margin-top: auto; /* This helps push the footer to the bottom */
-	}
-
-	.footer {
-		margin-top: 10px;
-		width: 100%;
-		padding: 2vw 6vw;
 		justify-content: space-around;
-		background: #333;
-		background: var(--tertiary-bg);
+		justify-content: space-evenly;
+		background: var(--third-bg);
 		color: #efefef;
 		display: flex;
+		position: relative; /* Add this to make absolute positioning work correctly */
 	}
 
-	.footer div {
-		text-align: center;
+	@media (max-width: 768px) {
+		.footer {
+			flex-direction: column;
+		}
 	}
 
 	.footer div h3 {
@@ -194,39 +256,36 @@
 		letter-spacing: 1px;
 	}
 
-	.col-1 a {
+	.col a {
+		/* padding: 0.25rem; */
+		height: 20px;
+		width: 20px;
 		display: block;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 0.25rem;
 		text-decoration: none;
-		color: #efefef;
+		color: var(--secondary-bg);
 		margin-bottom: 10px;
 	}
-
-	.col-1 a:hover {
-		color: #ee4141;
+	.social-icons a:hover {
+		color: var(--third-bg);
+		background-color: var(--accent-bright);
 	}
 
-	.col-2 {
-		flex-grow: 2;
-		padding-bottom: 3vw;
-		display: flex;
-		flex-direction: column;
+	.url-list a:hover {
+		color: var(--accent-bright);
+		/* background-color: var(--accent-bright); */
 	}
 
 	form input {
 		margin-bottom: 2vw;
-		width: 30vw;
-		height: 4vw;
-		border-radius: 4px;
 		text-align: center;
-		outline: none;
 		border: none;
-	}
-
-	input {
+		font-size: small;
 		background-color: var(--secondary-bg);
 		color: var(--secondary);
-		/* color: var(--primary); */
-		font-size: small;
 	}
 
 	form button {
@@ -234,7 +293,6 @@
 		padding: 1vw 2vw;
 		background-color: transparent;
 		border: 1vw;
-		/* outline: rgb(110, 106, 102) solid medium; */
 		outline: var(--secondary-darker) solid 1px;
 		outline: var(--primary-invert) solid 1px;
 		color: var(--primary-invert);
@@ -250,9 +308,16 @@
 	}
 
 	div.social-icons {
-		padding-top: 1vw;
+		/* padding-top: 1vw; */
 		display: flex;
 		justify-content: space-around;
+		justify-content: center;
+		column-gap: 1rem;
 		align-items: center;
+	}
+
+	.language-select:hover {
+		color: var(--accent-bright);
+		border-color: var(--accent-bright);
 	}
 </style>
