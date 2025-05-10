bd245af8a5124fbb8bc60fcdda9ec8fda470b3df
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-05-10 13:52:49 +0200

    remove comments and welcome message

--- src/stories/Header.svelte
+++ src/stories/Header.svelte
@@ -24,22 +24,11 @@
 		</a>
 		<div class="button-container">
 			{#if user}
-				<span class="welcome">
-					Welcome, <b>{user.name}</b>!
-				</span>
-				<Button hollow osl size="small" onclick={onLogout} label="Log out" style="neomorphic" />
-				<!-- <Button hollow size="small" onclick={onLogout} label="Log out" style="neomorphic" /> -->
+				<Button hollow osl size="small" onclick={onLogout} label="Logg ut" style="neomorphic" />
 			{:else}
-				<Button hollow osl size="small" onclick={onLogin} label="Log in" />
-				<Button hollow osl size="small" onclick={onCreateAccount} label="Sign up" />
-				<!-- <Button hollow size="small" onclick={onLogin} label="Log in" />
-			<Button hollow size="small" onclick={onCreateAccount} label="Sign up" /> -->
-
-				<!-- <NeumorphicButton hollow onClick={onLogin} label="Log in" /> -->
-				<!-- <NeumorphicButton size="small" onClick={onCreateAccount} label="Sign up" /> -->
-
-				<!-- <Button primary size="small" onClick={onCreateAccount} label="Sign up" /> -->
-				<!-- <Button primary size="small" onClick={onCreateAccount} label="Sign up" /> -->
+				<Button hollow osl size="small" onclick={onLogin} label="Logg inn" />
+				<!-- <Button hollow osl size="small" onclick={onCreateAccount} label="Sign up" /> -->
+				<Button hollow osl size="small" onclick={onCreateAccount} label="Registrer konto" />
 			{/if}
 		</div>
 	</div>
