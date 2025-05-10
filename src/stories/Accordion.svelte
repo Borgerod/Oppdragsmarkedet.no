5a7613d45c53b5bbb2ff4f46b80c39f089342d27
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-05-10 12:34:59 +0200

    fix accordion
    - add 'runes mode'-activation & set lang to 'ts'
    - change default limit from 'amount of tags' to 'amount of rows'
    - add 4 filled rows as default
    - add tooltip from Tooltip.svelte
    - change props; tooltip to tooltip_content
    - update css
    
    *now shows 4 full rows by default

--- src/stories/Accordion.svelte
+++ src/stories/Accordion.svelte
@@ -1,89 +1,50 @@
-<script>
+<svelte:options runes />
+
+<script lang="ts">
+	import './forms/form.css';
 	let isOpen = $state(false);
-	// TODO []: display tooltip at correct spot.
-	let showTooltip = $state(false);
-	const { title = '', tooltip = '' } = $props();
+	const { title = '', tooltip_content = '' } = $props();
+	import { tooltip } from './Tooltip.svelte';
 </script>
 
 <div class="accordion">
 	<button
 		class="accordion-header"
 		onclick={() => (isOpen = !isOpen)}
-		onmouseenter={() => (showTooltip = true)}
-		onmouseleave={() => (showTooltip = false)}
 		aria-expanded={isOpen}
+		use:tooltip={tooltip_content}
 	>
-		<h2>{title}</h2>
-		{#if tooltip && showTooltip}
-			<div class="tooltip">{tooltip}</div>
-		{/if}
-		<div class="chevron {isOpen ? 'open' : ''}"></div>
+		<h3>{title}</h3>
+
+		<div class="custom-chevron {isOpen ? 'open' : ''}"></div>
 	</button>
 
 	{#if isOpen}
-		<div class="accordion-content">
-			<slot />
-		</div>
+		<slot />
 	{/if}
 </div>
 
 <style>
-	.accordion h2 {
-		margin: 0;
-	}
-	.accordion {
-		border: 1px solid #e0e0e0;
-		border-radius: 8px;
-	}
-
-	.accordion-header {
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		border: none;
-		cursor: pointer;
-		transition: background 0.2s ease;
-	}
-
-	.chevron {
+	.custom-chevron {
 		width: 1rem;
 		height: 1rem;
 		position: relative;
 		transition: transform 0.2s ease;
 	}
 
-	.chevron::after {
+	.custom-chevron::after {
 		content: '';
 		position: absolute;
 		width: 0.5rem;
 		height: 0.5rem;
-		border-right: 2px solid #333;
-		border-bottom: 2px solid #333;
+		border-right: 2px solid var(--secondary);
+		border-bottom: 2px solid var(--secondary);
 		transform: rotate(45deg);
 		top: 0.2rem;
 		left: 0.25rem;
 	}
 
-	.chevron.open {
+	.custom-chevron.open {
 		transform: rotate(180deg);
-	}
-
-	.tooltip {
-		position: absolute;
-		/* background: #333; */
-		/* color: white; */
-		padding: 0.5rem;
-		border-radius: 4px;
-		font-size: 0.875rem;
-		top: 100%;
-		left: 50%;
-		transform: translateX(-50%);
-		white-space: nowrap;
-	}
-
-	.accordion-content {
-		/* padding: 1rem; */
-		/* background: white; */
 	}
 </style>
