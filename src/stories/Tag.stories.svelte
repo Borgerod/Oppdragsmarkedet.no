ce5460ff0050b99a079994cdc1ee993198ca7b93
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-06-04 10:21:40 +0200

    create tag story

---
+++ src/stories/Tag.stories.svelte
@@ -0,0 +1,38 @@
+<script lang="ts" module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Tag from './Tag.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Components/Tag',
+		component: Tag,
+		tags: ['autodocs'],
+		argTypes: {
+			label: {
+				control: 'object',
+				description: 'text to display in the tag'
+			},
+
+			color: {
+				control: { type: 'select', control: 'color' },
+				options: ['green', 'red', 'grey', 'accent', 'darkgrey']
+			},
+
+			textColor: {
+				control: { type: 'select', control: 'color' },
+				options: ['black', 'white']
+			}
+		},
+		args: {
+			label: 'Tag Label',
+			color: 'grey',
+			textColor: 'black'
+		}
+	});
+</script>
+
+<Story name="Green" args={{ label: 'Tag Label', color: 'green', textColor: 'black' }} />
+<Story name="Grey" args={{ label: 'Tag Label', color: 'grey', textColor: 'black' }} />
+<Story name="Red" args={{ label: 'Tag Label', color: 'red', textColor: 'black' }} />
+<Story name="accent" args={{ label: 'Tag Label', color: 'accent', textColor: 'black' }} />
+<Story name="darkgrey" args={{ label: 'Tag Label', color: 'darkgrey', textColor: 'black' }} />
---
+++ src/stories/Tag.svelte
@@ -0,0 +1,40 @@
+<svelte:options runes />
+
+<script lang="ts">
+	import '@stories/theme.css';
+	import '@src/app.css';
+	import '@stories/Tag.css';
+
+	// const props = $props<{
+	// 	label: string;
+	// 	// gridView: boolean;
+	// 	color: string;
+	// }>();
+
+	/**
+	 * @typedef {Object} Props
+	 * @property {string} [label] Button contents
+	 * @property {string} [color]
+	 * @property {string} [textColor]
+	 * @property {boolean} [shadow]
+	 * @property {boolean} [shadow]
+	 * @property {'small' | 'medium' | 'large'} [size] How large should the button be?
+	 **/
+	//  * @property {boolean} currency
+
+	/** @type {Props} */
+	const {
+		label = undefined,
+		color = 'button' as 'green' | 'red' | 'grey' | 'accent' | 'darkgrey',
+		textColor = 'button' as 'black' | 'white',
+		shadow = false,
+		size = 'medium'
+	} = $props();
+
+	let _shadow = shadow ? 'shadow' : '';
+</script>
+
+<div class={['tag', `${color}`, `text-${textColor}`, `${_shadow}`, `${size}`].join(' ')}>
+	<!-- class:_shadow = {shadow} -->
+	{label}
+</div>
