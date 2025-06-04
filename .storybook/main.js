0859b592edfcaeb1d4a11b986c6fdef353f6d13d
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-06-04 10:18:49 +0200

    config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,20 @@
-
-
 /** @type { import('@storybook/sveltekit').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|ts|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/sveltekit",
-    "options": {}
-  }
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+	addons: [
+		'@storybook/addon-essentials',
+		{
+			name: '@storybook/addon-svelte-csf',
+			options: {
+				legacyTemplate: true
+			}
+		},
+		'@chromatic-com/storybook',
+		'@storybook/experimental-addon-test'
+	],
+	framework: {
+		name: '@storybook/sveltekit',
+		options: {}
+	}
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,17 @@
+import '@src/color_scheme.css';
+import '@src/app.css';
+import '@stories/theme.css';
+
 /** @type { import('@storybook/svelte').Preview } */
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
 };
 
 export default preview;
