b0ceb9979cf1070639bcac2e9d6e778606d8e0c1
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-06-23 14:59:49 +0200

    add TagData

---
+++ src/lib/data/TagData.ts
@@ -0,0 +1,38 @@
+export const experienceRequirementsTags = [
+	{ label: 'Sveisebrev', value: 'sveisebrev' },
+	{ label: 'Asfaltering', value: 'asfaltering' },
+	{ label: 'Brønnboring', value: 'brønnboring' },
+	{ label: 'Drenering', value: 'drenering' },
+	{ label: 'Radon', value: 'radon' },
+	{ label: 'Sprenging', value: 'sprenging' },
+	{ label: 'Graving', value: 'graving' },
+	{ label: 'Gulv', value: 'gulv' },
+	{ label: 'Glassarbeid', value: 'glassarbeid' },
+	{ label: 'Termografering', value: 'termografering' },
+	{ label: 'Betong', value: 'betong' },
+	{ label: 'Vanntetting', value: 'vanntetting' },
+	{ label: 'Taktekking', value: 'taktekking' },
+	{ label: 'Steinlegger', value: 'steinlegger' },
+	{ label: 'Snørydding', value: 'snørydding' },
+	{ label: 'Hagestell', value: 'hagestell' }
+];
+export const fieldTags = experienceRequirementsTags;
+
+export const jobAttributesTags = [
+	// { label: 'Stort prosjekt', value: 'large_project' },
+	{ label: 'Lite prosjekt', value: 'lite_prosjekt' },
+	{ label: 'Stort prosjekt', value: 'stort_prosjekt' },
+	{ label: 'Enmannsjobb', value: 'enmannsjobb' },
+	{ label: 'Flermannsjobb', value: 'flermannsjobb' },
+	{ label: 'Haster', value: 'haster' },
+	{ label: 'Innejobb', value: 'innejobb' },
+	{ label: 'Utejobb', value: 'utejobb' },
+	{ label: 'Trapp', value: 'trapp' },
+	{ label: 'Kjøretøy tilgang', value: 'kjøretøy_tilgang' }
+];
+
+export const clientRoleTags = [
+	{ label: 'Privat', value: 'private' },
+	{ label: 'Bedrift', value: 'business' },
+	{ label: 'Statlig', value: 'government' }
+];
