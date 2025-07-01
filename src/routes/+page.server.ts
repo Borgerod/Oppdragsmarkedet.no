2e00176e1277d645a270113327984a96b3551540
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2025-07-01 21:33:25 +0200

    Update +page.server.ts
    delete comments

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,29 +1,3 @@
-// import { fail, redirect } from '@sveltejs/kit';
-// import { deleteSessionTokenCookie, invalidateSession } from '$lib/server/session';
-
-// import type { Actions, RequestEvent } from './$types';
-
-// export async function load(event: RequestEvent) {
-// 	if (event.locals.session === null || event.locals.user === null) {
-// 		return redirect(302, '/login');
-// 	}
-// 	return {
-// 		user: event.locals.user
-// 	};
-// }
-
-// export const actions: Actions = {
-// 	default: action
-// };
-
-// async function action(event: RequestEvent) {
-// 	if (event.locals.session === null) {
-// 		return fail(401);
-// 	}
-// 	invalidateSession(event.locals.session.id);
-// 	deleteSessionTokenCookie(event);
-// 	return redirect(302, '/login');
-// }
 import { fail, redirect } from '@sveltejs/kit';
 import { deleteSessionTokenCookie } from '$lib/server/session';
 import { db } from '@db/index';
